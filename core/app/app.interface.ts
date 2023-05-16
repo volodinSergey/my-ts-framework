dd869d121cd79335a4a7e350702aabb67b8e5e1f
Volodin Sergey <volodin7ergey@icloud.com>
2023-05-16 16:51:52 +0300

    [docs/core-app]: added docs to IAppOptions interface

--- core/app/app.interface.ts
+++ core/app/app.interface.ts
@@ -1,7 +1,12 @@
+
 export interface IApp {
 	run(): void
 }
 
+/**
+ * @title IAppOptions
+ * @description Describes options object during app initialization
+ */
 export interface IAppOptions {
 	rootElement: string
 }
