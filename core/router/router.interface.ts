dd67ff1f6a976b6ff42c7836f8c6fff4fce35491
Volodin Sergey <volodin7ergey@icloud.com>
2023-05-16 04:46:16 +0300

    [docs/router]: added docs to IRouterOptions interface

--- core/router/router.interface.ts
+++ core/router/router.interface.ts
@@ -1,5 +1,9 @@
 import { IBaseComponent } from '@core/component/base-component.interface'
 
+/**
+ * @title IRouterOptions
+ * @description Describes options object during router initialization
+ */
 export interface IRouterOptions {
 	rootElement: HTMLElement
 	routes: IRoute[]
