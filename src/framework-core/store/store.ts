1adee6f90ee713fac4b25f5b960a8925a90e3024
Volodin Sergey <volodin7ergey@icloud.com>
2023-05-30 17:57:37 +0300

    [docs-core/store]: added docs for store class

--- src/framework-core/store/store.ts
+++ src/framework-core/store/store.ts
@@ -2,10 +2,13 @@ import { IBaseComponent } from "@core/component/base-component.abstract.interfac
 import { IStore, IStoreState } from "./store.interface"
 
 export class Store<T extends IStoreState> implements IStore {
+    /* Components list that will be observers and react to store changes */
     #observers: IBaseComponent[] = []
+
+    /* Store state that each store instance will have. Custom state must be extended by base StoreState*/
     #state: T
 
-    constructor(state: T ) {
+    constructor(state: T) {
         this.#state = new Proxy(state, {
             get(target, prop: string) {
                 return target[prop]
@@ -26,6 +29,10 @@ export class Store<T extends IStoreState> implements IStore {
         this.#observers.push(component)
     }
 
+    /**
+     * @title Method [ #notify ]
+     * @description Triggers method update in all observers then state changes
+     */
     #notify(): void {
         this.#observers.forEach(observer => observer.update ? observer.update() : null)
     }
