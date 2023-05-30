dbc0f1a40de8691a29303beba2e89c9e8eb02da7
Volodin Sergey <volodin7ergey@icloud.com>
2023-05-30 04:35:51 +0300

    [docs/core-base-component]: added docs to BaseComponent abstract class

--- src/framework-core/component/base-component.abstract.ts
+++ src/framework-core/component/base-component.abstract.ts
@@ -2,10 +2,12 @@ import { Compiler } from '../compiler/compiler';
 import { IBaseComponent } from './base-component.abstract.interface';
 
 export abstract class BaseComponent implements IBaseComponent {
-	protected readonly props: string | undefined
+	protected readonly props: string | undefined // !TOOO : props must be an empty object by default. NOT STRING OR UNDEFINED
+
+	/*Each component has built-in compiler */
 	readonly compiler
 
-	constructor(props?: string) {
+	constructor(props?: string) { // !TOOO : props must be an empty object by default. NOT STRING OR UNDEFINED
 		this.props = props
 		this.compiler = new Compiler()
 	}
